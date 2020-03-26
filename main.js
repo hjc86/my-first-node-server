c9440ba1a5b14319daa4f3508de2b45b0949cf21
Helder Carmen <heldercarmen@gmail.com>
2020-03-26 16:21:45 +0000

    clean up main.js and server.js

--- main.js
+++ main.js
@@ -1,46 +1,7 @@
-// require('http')
-// var url = require('url');
-// var fetch = require('node-fetch')
-
-
-// $(document).ready(function(){
-
-
-
-//     async function callUnsplash() {
-//         var response1 = await fetch("https://source.unsplash.com/500x500/?water").then(response=> {return response.url});
-//         var response2 = await fetch("https://source.unsplash.com/500x500/?horse").then(response=> {return response.url});
-        
-//         var pics = [response1, response2];
-//         // console.log(pics)
-//         return pics
-//     };
-    
-//     callUnsplash().then(response => { 
-//         $('#img1').attr("src", response[0]);
-//         $('#img2').attr("src", response[1]);
-
-//     })   
-
-// });
-
-
 function getUnsplashImg(subject){
     let base_url = "https://source.unsplash.com/500x500/?";   
     return fetch(base_url + subject);
 }
-
-// function getUnsplashImg(subject){
-//     const req = new XMLHttpRequest()
-//     let base_url = "https://source.unsplash.com/500x500/?";  
-//     return new Promise(resolve=>{
-//             let req = new XMLHttpRequest(); 
-//             req.open("GET", base_url+subject);
-//             resolve(req.send());
-//         }
-// )}
-
-
 
 async function getImages(){ 
     pic_array =[await getUnsplashImg("water"), await getUnsplashImg("fire")];
@@ -56,5 +17,3 @@ $(document).ready(function(){
         console.log(response)
     });
 })  
-
-
--- server.js
+++ server.js
@@ -18,18 +18,6 @@ http.createServer(function(req, res) {
         reversedBody =  bodyObject.string1.split('').reverse().join('');
       
         res.end("this is our text: " + reversedBody); 
-
-
-    
     });
-
-    // we write in the head of response what the conetent text is so the requester (client) knows what to expect
     
-//    console.log("this is in server:", body)
-    
-    // res.writeHead(200, {'Content-Type':'text/html'}); 
-    
-    // // we need to end the respsonse ALWAYS
-    // res.end("this is our text: " + body);
-
 }).listen(8080);
