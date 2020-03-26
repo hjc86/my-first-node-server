20107b9460f7fdb863a51734f84b955fe6e5a21d
Helder Carmen <heldercarmen@gmail.com>
2020-03-26 20:57:09 +0000

    added html for orignal message and styling

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ const { parse } = require('querystring');
 
 http.createServer(function(req, res) {
 
-    let body = '';
+    let body='';
+
+
     req.on('data', chunk => {
         body += chunk.toString(); // convert Buffer to string
     });
     req.on('end', () => {
-        res.writeHead(200, {'Content-Type':'text/html'}); 
+        // res.writeHead(200, {'Content-Type':'text/html'}); 
         let bodyObject = parse(body);        
         let reversedBody= bodyObject.string1.split('').reverse().join('');
-        res.end(`<h1 style="background-color: red; text-align: center;"> this is our reversed text: ${reversedBody} </h1>`); 
+        res.end(
+            `<h1 style="background-color: red; text-align: center;"> this is our reversed text: ${reversedBody} </h1>
+            <h1 style="background-color: green; text-align: center;"> this is our original text: ${bodyObject.string1} </h1>
+            
+            `); 
     });
 
 }).listen(8080);
