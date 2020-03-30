8ae70ad6480f23e5c266d52e864742975fd8bd8a
Helder Carmen <heldercarmen@gmail.com>
2020-03-30 09:25:15 +0100

    update files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-ECHO is on.
+# Name of Exercise
+
+my-first-node-server
+
+## Your Name(s) 
+
+Helder and Keir
+
+### What I/we have learned
+
+
+### What challenges I/we overcame
+### What I/we struggled with
+### What I/we would do differently next time
--- server.js
+++ server.js
@@ -6,17 +6,22 @@ http.createServer(function(req, res) {
 
     let body='';
 
+    if
+
 
     req.on('data', chunk => {
         body += chunk.toString(); // convert Buffer to string
     });
+
+
     req.on('end', () => {
-        // res.writeHead(200, {'Content-Type':'text/html'}); 
+        res.writeHead(200, {'Content-Type':'text/html',
+                           "Access-Control-Allow-Origin": "*"}); 
         let bodyObject = parse(body);        
         let reversedBody= bodyObject.string1.split('').reverse().join('');
         res.end(
-            `<h1 style="background-color: red; text-align: center;"> this is our reversed text: ${reversedBody} </h1>
-            <h1 style="background-color: green; text-align: center;"> this is our original text: ${bodyObject.string1} </h1>
+            `<h1 style="color: red; text-align: center;"> this is our reversed text: ${reversedBody} </h1>
+            <h1 style="color: green; text-align: center;"> this is our original text: ${bodyObject.string1} </h1>
             
             `); 
     });
